5d5cdc55325b4b50cec00e225e3e8bec711b08b4
dip497 <sharmadipendra@jklu.edu.in>
2021-12-03 10:42:54 +0530

    k

--- app/src/main/java/com/example/labexam/recycler.java
+++ app/src/main/java/com/example/labexam/recycler.java
@@ -1,4 +1,22 @@
 package com.example.labexam;
 
-public class recycler {
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class recycler extends RecyclerView.ViewHolder {
+
+    ImageView Image;
+    View view;
+
+    recycler(View itemView) {
+
+        super(itemView);
+        Image = (ImageView)itemView
+                .findViewById(R.id.image_view_recycler);
+
+        view  = itemView;
+    }
 }
---
+++ app/src/main/java/com/example/labexam/recyclerAdapter.java
@@ -0,0 +1,59 @@
+package com.example.labexam;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.Collections;
+import java.util.List;
+
+public class recyclerAdapter extends RecyclerView.Adapter<recycler> {
+    List<recycler> list
+            = Collections.emptyList();
+
+    Context context;
+
+
+    public recyclerAdapter(List<recycler> list,
+                                Context context)
+    {
+        this.list = list;
+        this.context = context;
+    }
+    @NonNull
+    @Override
+    public recycler onCreateViewHolder(@NonNull ViewGroup parent, int i) {
+        Context context
+                = parent.getContext();
+        LayoutInflater inflater
+                = LayoutInflater.from(context);
+
+        // Inflate the layout
+
+        View photoView
+                = inflater
+                .inflate(R.layout.custom_card,
+                        parent, false);
+
+        recycler viewHolder
+                = new recycler(photoView);
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recycler recycler, int i) {
+        final index = recycler.getAdapterPosition();
+        recycler.Image
+                .setImageResource(list.get(i).Image);
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return list.size();
+    }
+}
